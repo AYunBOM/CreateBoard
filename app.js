ea0cb5f1fc625f5b158e54a12602e90d50a94bbe
AYunBOM <119410207+AYunBOM@users.noreply.github.com>
2023-05-25 15:45:47 +0900

    엔터값으로 입력받은 값 출력하기

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ var app = express();
 
 const maria = require('./maria');
 maria.connect();
+
+function printName()  {
+    const name = document.getElementById('name').value;
+    document.getElementById("result").innerText = name;
+  }
