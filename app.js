20a5bf5d47978d76a1ad4ce58cb340f61048c6cd
AYunBOM <119410207+AYunBOM@users.noreply.github.com>
2023-05-31 15:42:25 +0900

    입력한 데이터 값 post로 받아와 mariaDB에 INSERT 하기

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
 const express = require('express');
-
-var app = express();
-
 const maria = require('./maria');
+var http = require('http');
+var app = express();
+const indexRouter = require('./index');
+const port = 8080;
 
-maria.connect(function(err){
-    if (err) throw err;
-    console.log("You are connected");
-});
+app.use('/', indexRouter);
 
-// const info = {
-//     "name": '영주',
-//     "title": '공주병',
-//     "detail" : '말기인 듯 하다.',
-//     "ID" : '101'
-// };
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-$title = $_POST["title"];
-$name =$_POST["name"];
-$detail =$_POST["detail"];
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
 
-var sql = "INSERT INTO test(username, title, detail) VALUES('$name','$title','$detail',?)";
-//var params = [info['name'], info['title'], info['detail'], info['ID']]
-maria.query(sql, params, function(err, rows, fields){
-    if(err){
-        console.log(err);
-    } else{
-        console.log(rows.name);
-    }
-});
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+})
+
+app.post('/post', function(req, res) {
+    var username = req.body.username;
+    var title = req.body.title;
+    var detail = req.body.detail;
+     
+    var sql = "INSERT INTO post(username, title, detail) VALUES(?,?,?)";
+    var params = [username, title, detail];
+
+    console.log(params);
+    
+    maria.query(sql, params, (err, rows) => {
+        if(err){
+            console.log(err);
+        } else{
+            console.log(rows.username);
+        }
+        });
+    });
 
 // 연결 종료
-maria.end();
+//maria.end();
