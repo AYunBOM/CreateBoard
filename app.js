3325dafe87bee4ab57f98dff5f5c865a4cba0b74
AYunBOM <119410207+AYunBOM@users.noreply.github.com>
2023-06-19 19:19:02 +0900

    최종본(제출용)

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var path = require('path');
 var app = express();
 const port = 8080;
 var page_num = 5;
+
 // router 설정
 const indexRouter = require('./index');
+app.use('/', indexRouter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,15 +17,13 @@ app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-app.use('/', indexRouter);
-
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
 })
 
 /* GET home page. */
 app.get('/', function(req, res, next) {
-    res.redirect('/1')
+    res.redirect('/1');
 }); //localhost:8080으로 메인페이지 열기
 
 app.get('/write', function(req, res) {
@@ -32,27 +32,24 @@ app.get('/write', function(req, res) {
 
 app.get('/:pageNum', function(req, res, next) {
     var pageNum = req.params.pageNum;
-    if (pageNum < 1)
-        pageNum = 1
-
+    if (pageNum < 1){
+        pageNum = 1;
+    }
     if (isNaN(req.query.page_num)==false) {
         page_num = parseInt(req.query.page_num)
-        if (page_num <1)
-            page_num = 5
-        pageNum = 1
+        if (page_num <1){
+            page_num = 5;
+        }
+        pageNum = 1;
     }
-    
-    var sql = "SELECT username, title, detail, date_format(todate,'%Y-%m-%d %H:%i') todate, userID FROM post;";
 
-    maria.query(sql, (err, rows) => {
+    maria.query("SELECT username, title, detail, date_format(todate,'%Y-%m-%d %H:%i') todate, userID FROM post;", (err, rows) => {
         if(err){console.log(err);}
         else{
             const arr = [];
-            
             for(var i = rows.length-1; i>=0; i--){
                 arr.push([rows[i].userID, rows[i].title, rows[i].username, rows[i].todate]);
             }
-            //console.log(arr);
             res.render('main.ejs', {data: arr, page: pageNum, length: rows.length-1, page_num: page_num, pass:true});
         }
     });
@@ -64,9 +61,7 @@ app.post('/write', function(req, res) {
     var detail = req.body.detail;
     var params = [username, title, detail];
     
-    var sql = "INSERT INTO post(username, title, detail) VALUES(?,?,?)";
-    
-    maria.query(sql, params, (err, rows) => {
+    maria.query("INSERT INTO post(username, title, detail) VALUES(?,?,?)", params, (err, rows) => {
         if(err){console.log(err);}
     });
     res.redirect('/1')
@@ -85,23 +80,23 @@ app.get('/read/:userID',function(req,res){
             });
         }
     });
-});
+});//게시글 별 댓글 등록하기
 
 app.get("/read/delete/:userID", function (req, res) {
     maria.query('delete from commboard where userID = ?', req.params.userID, (err, rows) => {
         maria.query('delete from post where userID = ?', req.params.userID, (err, rows) => {
             res.redirect('/1');
         });
-    })
-});
+    });
+});//게시글 삭제&해당 게시글에 등록되어있는 댓글도 삭제
 
 app.get("/read/delete2/:commID", function (req, res) {
     maria.query('select userID from commboard where commID = ?', req.params.commID, (err, userID) => {
         maria.query('delete from commboard where commID = ?', req.params.commID, (err, rows) => {
             res.redirect('/read/'+userID[0].userID);
-        })
-    })
-});
+        });
+    });
+});//게시글 별 댓글 삭제
 
 app.post("/read/:userID", function (req, res) {
     var commname = req.body.commname;
@@ -109,15 +104,11 @@ app.post("/read/:userID", function (req, res) {
     const userID = req.body.userID;
     var params = [commname, comm, userID];
 
-    var sql = "INSERT INTO commboard(commname, comm, userID) VALUES(?,?,?)";
-    
-    maria.query(sql, params, (err, rows) => {
+    maria.query("INSERT INTO commboard(commname, comm, userID) VALUES(?,?,?)", params, (err, rows) => {
         if(err){console.log(err);}
         res.redirect('/read/'+userID);
     });
-});
-
-
+});//댓글 상세정보 가지고 오기
 
 // 연결 종료
 //maria.end();
